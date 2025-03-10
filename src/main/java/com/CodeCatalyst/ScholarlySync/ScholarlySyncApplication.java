33ba042326d47c8d391362b1969a1163ed6180c1
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-03-10 17:38:38 +0200

    First commit

---
+++ src/main/java/com/CodeCatalyst/ScholarlySync/ScholarlySyncApplication.java
@@ -0,0 +1,13 @@
+package com.CodeCatalyst.ScholarlySync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScholarlySyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScholarlySyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodeCatalyst/ScholarlySync/ScholarlySyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeCatalyst.ScholarlySync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScholarlySyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
