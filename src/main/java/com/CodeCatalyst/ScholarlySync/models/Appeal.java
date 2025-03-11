66f8173d34d2bcd78c8869933b1de327f6110d24
Christian Isingizwe <bizimanaisingizwechristian@gmail.com>
2025-03-11 19:04:47 +0200

    Some changes made

--- src/main/java/com/CodeCatalyst/ScholarlySync/models/Appeal.java
+++ src/main/java/com/CodeCatalyst/ScholarlySync/models/Appeal.java
@@ -1,13 +1,11 @@
 package com.CodeCatalyst.ScholarlySync.models;
 
 import com.CodeCatalyst.ScholarlySync.enums.AppealStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.yaml.snakeyaml.error.Mark;
 
 @Entity
 @Data
@@ -19,9 +17,16 @@ public class Appeal {
     @Id
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "student_id")
     private User student;
+
+    @ManyToOne
+    @JoinColumn(name = "mark_id")
     private Marks mark;
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private AppealStatus status;
 
 }
